07c304b47557a8b9ad003507d674ba9c46f2aef5
Lab_Orange_ <Lab_Orange_@DESKTOP-28C0TIH>
2018-05-10 16:47:30 +0300

    connstring

--- Sucuri/Form1.cs
+++ Sucuri/Form1.cs
@@ -14,6 +14,7 @@ namespace Sucuri
     {
         public Form1()
         {
+            // connectionString="Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True"
             InitializeComponent();
         }
 
